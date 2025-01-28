87cec379ce52e67a8c63931f486eb0fed789ac3f
lagauzere <hugolagauzere@gmail.com>
2025-01-28 23:48:30 +0100

    [#20] fix: CamelCaseClasses

--- backend/app/View/Components/menu.php
+++ backend/app/View/Components/menu.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class menu extends Component
+class Menu extends Component
 {
     /**
      * Create a new component instance.
