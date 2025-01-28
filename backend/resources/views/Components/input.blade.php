02c5c6be6e6e4c2cb6ba8ec413120d6878f32dbe
Hugo Lagauzere <hugo.lagauzere@etu.unicaen.fr>
2025-01-28 14:49:56 +0100

    Resolve "Connexion front"

---
+++ backend/resources/views/Components/input.blade.php
@@ -0,0 +1,4 @@
+@props(['type', 'name', 'placeholder'])
+<div>
+    <input class="rounded-cards text-h4 border-10 px-4 py-2 " type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}">
+</div>
