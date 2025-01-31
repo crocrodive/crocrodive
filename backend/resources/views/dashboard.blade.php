d0d6e315d83c8cbea8478bf9e8928a1c17de7094
Guillaume Bergerot <guillaume.bergerot@unicaen.fr>
2025-01-31 10:54:23 +0100

    Resolve "no welcome view"

--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -1,9 +1,7 @@
 <x-layout>
-    <div class="flex flex-row h-full w-full ">
-        <div class="flex flex-col items-center w-full h-full bg-background-200">
-            <pre>
-            {{var_dump(Auth::user()->role_id)}}
-            </pre>
+    <div class="flex flex-row h-full w-full">
+        <div class="flex flex-col items-center justify-center w-full h-full bg-background-200">
+            <h1 class="text-4xl font-bold text-center mt-10">Bonjour, {{ Auth::user()->user_firstname }} {{ Auth::user()->user_lastname }} 👋</h1>
         </div>
     </div>
 </x-layout>
