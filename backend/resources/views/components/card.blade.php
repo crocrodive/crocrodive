f73d1767c0bce75a6b28e384fbee634c10308407
SirPunisher <brissaudandgel@gmail.com>
2025-01-31 10:11:12 +0100

    hot fix: car blade (regression fix)

--- backend/resources/views/components/card.blade.php
+++ backend/resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div class="flex flex-col min-w-[360px] min-h-[168px] bg-background-100 border-2 border-background-300 rounded-image shadow-buttonBlack hover:-translate-y-1 transition-transform duration-300 justify-center">
+<div  {{ $attributes->merge(['class' => '"flex flex-col min-w-[360px] min-h-[168px] bg-background-100 border-2 border-background-300 rounded-image shadow-buttonBlack hover:-translate-y-1 transition-transform duration-300 justify-center"']) }}>
     {{ $slot }}
 </div> 
